87367091b571faf75c61fca079ab688917ea9cde
Stefan Rudvin <Stefan.rudvin@hotmail.com>
2016-11-02 13:53:43 +0000

    Passed post creation test

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -10,4 +10,15 @@ feature 'Creating posts' do
     expect(page).to have_content('#Coffeetime')
     expect(page).to have_css("img[src*='coffee.jpg']")
   end
+  
+  scenario 'needs an image to create a post' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Caption', with: 'nom nom nom #Coffeetime'
+    click_button 'Create Post'
+    expect(page).to have_content('Halt, you fiend! You need an image to post here!')
+  end
+  
+  
+  
 end
