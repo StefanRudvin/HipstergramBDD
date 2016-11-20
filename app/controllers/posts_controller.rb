fcf6ac67c40e02a58b71c346b29864c361b28447
Stefan Rudvin <Stefan.rudvin@hotmail.com>
2016-11-20 22:15:40 +0000

    Kaminari works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,21 @@
 class PostsController < ApplicationController
-  
-  before_action :set_post, only: [:show, :edit, :update, :destroy]  
+
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
-  before_action :owned_post, only: [:edit, :update, :destroy] 
+  before_action :owned_post, only: [:edit, :update, :destroy]
   before_action :loggedin
-  
+
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC').page params[:page]
   end
-  
+
   def show
   end
-  
+
   def new
     @post = current_user.posts.build
   end
-  
+
   def create
     @post = current_user.posts.build(post_params)
 
@@ -27,7 +27,7 @@ class PostsController < ApplicationController
       render :new
     end
   end
-  
+
   def update
     if @post.update(post_params)
       flash[:success] = "Post updated hombre"
@@ -37,35 +37,35 @@ class PostsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     @post.destroy
     flash[:success] = 'Problem solved!  Post deleted.'
     redirect_to posts_path
   end
-  
+
   def edit
   end
 
   private
-  
+
   def loggedin
     @loggedin = current_user
   end
-  
+
   def post_params
     params.require(:post).permit(:image, :caption)
   end
-  
+
   def set_post
     @post = Post.find(params[:id])
   end
-  
-  def owned_post  
+
+  def owned_post
     unless current_user == @post.user
       flash[:alert] = "That post doesn't belong to you!"
       redirect_to root_path
     end
-  end 
+  end
 
 end
