84201fc297ac5eaa3453048eeaa17515066c4fe7
Stefan Rudvin <Stefan.rudvin@hotmail.com>
2016-11-21 00:33:42 +0000

    User profile pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-module ApplicationHelper  
+module ApplicationHelper
   def alert_for(flash_type)
     {
         :success => 'alert-success',
@@ -7,11 +7,19 @@ module ApplicationHelper
         :notice => 'alert-info'
     }[flash_type.to_sym] || flash_type.to_s
   end
-  
-  def form_image_select(post)  
+
+  def form_image_select(post)
     return image_tag post.image.url(:medium),
                      id: 'image-preview',
                      class: 'img-responsive' if post.image.exists?
     image_tag 'beach.jpg', id: 'image-preview', class: 'img-responsive'
   end
-end  
+end
+
+def profile_avatar_select(user)
+  return image_tag user.avatar.url(:medium),
+                   id: 'image-preview',
+                   class: 'img-responsive img-circle profile-image' if user.avatar.exists?
+  image_tag 'llama.jpg', id: 'image-preview',
+                                  class: 'img-responsive img-circle profile-image'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-  
+
   validates :user_name, presence: true, length: { minimum: 4, maximum: 12 }
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+
   has_many :comments, dependent: :destroy
-  
+
+  has_attached_file :avatar, styles: { medium: '152x152#' }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
 end
